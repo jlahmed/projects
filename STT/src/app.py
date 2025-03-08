3cd847425084417061339b27f79d08699a6cee0d
Jubayer Ahmed <jlahmed23@gmail.com>
2025-03-08 02:07:46 -0700

    added open ai and STT methods to app.py

--- STT/src/app.py
+++ STT/src/app.py
@@ -1,8 +1,52 @@
-import streamlit as st
-import openai
-import langchain
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
-import speech_recognition as sr
-from gtts import gTTS
-import os
+import os  
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser 
+import azure.cognitiveservices.speech as speechsdk
+
+def ask(prompt, client): 
+    response = client.chat.completions.create(
+        model="gpt-4o", # model = "deployment_name".
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. Keep your answers short and to the point"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    print(response.choices[0].message.content)
+
+def listen():
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    speech_config = speechsdk.SpeechConfig(subscription=os.getenv("SPEECH_KEY"), region=os.getenv("SPEECH_REGION"))
+    speech_config.speech_recognition_language="en-US"
+
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    print("Speak into your microphone.")
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        promt = speech_recognition_result.text
+        print("Recognized: {}".format(promt))
+        return promt
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
+
+if __name__ == "__main__":
+    
+    load_dotenv()
+
+    llm = AzureOpenAI(
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_version="2024-02-01"
+    )
+
+    ask(prompt="what is the color of the sky", client=llm)
