3fd9b312bc48fa9d306e3e305e88cdb66e609c23
Jubayer Ahmed <jlahmed23@gmail.com>
2025-03-09 03:34:42 -0600

    added code to use azure open ai with langchain

--- STT/src/app.py
+++ STT/src/app.py
@@ -1,19 +1,60 @@
 import os  
 from dotenv import load_dotenv
-from openai import AzureOpenAI
+from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.prompts import PromptTemplate
+from openai import AzureOpenAI
 from langchain_core.output_parsers import StrOutputParser 
 import azure.cognitiveservices.speech as speechsdk
+from langchain_core.messages import HumanMessage, SystemMessage
 
-def ask(prompt, client): 
-    response = client.chat.completions.create(
-        model="gpt-4o", # model = "deployment_name".
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant. Keep your answers short and to the point"},
-            {"role": "user", "content": prompt}
-        ]
+if __name__ == "__main__":
+    
+    load_dotenv()
+
+    # Without Langchain
+    # llm = AzureOpenAI(
+    # azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+    # api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    # api_version="2024-02-01"
+    # )
+
+    llm = AzureAIChatCompletionsModel(
+    endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    credential=os.environ["AZURE_OPENAI_API_KEY"]
     )
-    print(response.choices[0].message.content)
+
+    system_template = "Translate the following into {language}:"
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", "{text}")]
+    )
+
+    parser = StrOutputParser()
+
+    chain = prompt_template | llm | parser
+
+    chain.invoke({"language": "italian", "text": "hi"})
+
+    #ask(prompt="what is the color of the sky", llm=llm)
+
+
+def ask(prompt, llm): 
+    messages = [
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content=prompt),
+    ]
+
+    llm.invoke(messages)
+
+    # Without Langchain
+    # response = llm.chat.completions.create(
+    #     model="gpt-4o", # model = "deployment_name".
+    #     messages=[
+    #         {"role": "system", "content": "You are a helpful assistant. Keep your answers short and to the point"},
+    #         {"role": "user", "content": prompt}
+    #     ]
+    # )
+    # print(response.choices[0].message.content)
 
 def listen():
     # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
@@ -38,15 +79,3 @@ def listen():
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             print("Error details: {}".format(cancellation_details.error_details))
             print("Did you set the speech resource key and region values?")
-
-if __name__ == "__main__":
-    
-    load_dotenv()
-
-    llm = AzureOpenAI(
-    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    api_version="2024-02-01"
-    )
-
-    ask(prompt="what is the color of the sky", client=llm)
