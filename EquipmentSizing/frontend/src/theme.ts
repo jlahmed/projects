2532a2e238829474b09798804d2b9df378f0b139
Jubayer Ahmed <jlahmed23@gmail.com>
2025-06-21 20:18:50 -0600

    Add EquipmentSizing frontend project files

---
+++ EquipmentSizing/frontend/src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#ffffff',
+      paper: '#ffffff',
+    },
+  },
+}); 
